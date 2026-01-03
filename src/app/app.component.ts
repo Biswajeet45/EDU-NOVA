cfa9843e3db3d7ff0f9231524faeaa18a8d6cae2
Biswajeet45 <biswajeetsahu09@gmail.com>
2026-01-03 11:14:55 +0530

    Small changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ import { VideoComponent } from './components/video/video.component';
     VideoComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-dream-app';
